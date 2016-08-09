2f87123b51d448b2776ec553c5272810300876e1
Chen <dehua.chen@sga.pucminas.br>
2016-08-09 11:26:28 -0300

    teste

--- app/src/main/java/com/cdh/mynote/MainActivity.java
+++ app/src/main/java/com/cdh/mynote/MainActivity.java
@@ -3,6 +3,7 @@ package com.cdh.mynote;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Hello Asd!", Toast.LENGTH_SHORT).show();
 
         startService(new Intent(this, ServiceChatHead.class));
     }
