3ec53232dbdd8391d55290519daee779168f6ea6
cc <dehua.chen@sga.pucminas.br>
2016-08-09 23:18:56 -0300

    teste: corrigir o erro no nome de usuario(Github).

--- app/src/main/java/com/cdh/mynote/MainActivity.java
+++ app/src/main/java/com/cdh/mynote/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
                 new Stuff("ola", "09/08"),
                 new Stuff("????", "09/08"), };
 
+
         ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(new MyAdapter(this, stuffs));
     }
